f711d8f11fdccf38655a55d50442adbe3ae62a7c
SIDELNIKOV Ilya <sidelnikov.ilya@gmail.com>
2018-06-26 22:34:54 +0200

    Version finale service

---
+++ src/AppBundle/Service/FlightInfo.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder
+ * Date: 24/06/18
+ * Time: 19:01
+ */
+
+namespace AppBundle\Service;
+
+
+class FlightInfo
+{
+    /**
+     * @var string
+     */
+    private $unit;
+
+    /**
+     * Constucteur
+     *
+     * @param string $unit Defined in config.yml
+     */
+    public function __construct($unit)
+    {
+        $this->unit = $unit;
+    }
+
+    /**
+     * Distance calculation between latitude/longitude based on Harnive's formula
+     * http://www.codecodex.com/wiki/Calculate_Distance_Between_Two_Points_on_a_Globe#PHP
+     *
+     * @param float $latitudeFrom  Departure
+     * @param float $longitudeFrom Departure
+     * @param float $latitudeTo    Arrival
+     * @param float $longitudeTo   Arrival
+     *
+     * @return float
+     */
+    public function getDistance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo)
+    {
+        $d = 0;
+        $earth_radius = 6371;
+        $dLat = deg2rad($latitudeTo - $latitudeFrom);
+        $dLon = deg2rad($longitudeTo - $longitudeFrom);
+
+        $a = sin($dLat/2) * sin($dLat/2) + cos(deg2rad($latitudeFrom)) * cos(deg2rad($latitudeTo)) * sin($dLon/2) * sin($dLon/2);
+        $c = 2 * asin(sqrt($a));
+
+        switch ($this->unit) {
+            case 'km':
+                $d = $c * $earth_radius;
+                break;
+            case 'mi':
+                $d = $c * $earth_radius / 1.609344;
+                break;
+            case 'nmi':
+                $d = $c * $earth_radius / 1.852;
+                break;
+        }
+
+        return $d;
+    }
+
+    /**
+     * Time calculation
+     * @param float $distance  Distance
+     * @param integer $cruiseSpeed CruiseSpeed
+     *
+     * @return float
+     */
+
+    public function getTime($distance,$cruiseSpeed)
+    {
+        $dist = $distance;
+        $speed = $cruiseSpeed;
+        $time = $dist/$speed;
+
+
+        return $time;
+
+    }
+
+}
